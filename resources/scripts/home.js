98e678e671f0f96c584df7c51c21fb47a0a96cf2
Fate1405 <58146913+Fate1405@users.noreply.github.com>
2021-11-02 19:11:01 +0100

    add visitor counter

---
+++ resources/scripts/home.js
@@ -0,0 +1,11 @@
+if (!(localStorage.getItem("count"))) {
+    localStorage.setItem("count", "0");
+}
+
+let n = parseInt(localStorage.getItem("count")) + 1;
+
+localStorage.setItem("count", n.toString());
+
+const disp = document.getElementById("visitorcount");
+
+disp.innerHTML = `Number of visits: ${localStorage.getItem("count")}`;
